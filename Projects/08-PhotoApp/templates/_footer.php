f8ba2746e361b4b5d38f99cf1203ac21117d9d5d
Sweatergawd <mrmcneill1541@gmail.com>
2014-11-17 11:56:35 -0500

    update to gallery and tabet view

---
+++ Projects/08-PhotoApp/templates/_footer.php
@@ -0,0 +1,66 @@
+<footer>
+	<div class="row">
+		
+				<section class="mobile-full tablet-full desktop-7 centered">
+						<h4 class="final_header">Download Now</h4>
+						<p class="feature_details">Nullam id dolor id nibh ultricies vehicula deposit quid pro quo hic escorol. </p>
+				</section>
+	</div>
+			
+				
+
+				<div class="row">
+					<!-- <div id=" mobile-full desktop-8 desktop-push-2 magnets"> !-->
+						
+						<div class="mobile-3 tablet-full desktop-5 centered">
+							
+								<a href="www.twitter.com" id="apple_" >Apple</a>   
+								<a href="www.facebook.com" id="google_play" >Google Play Store</a> 
+								<a href="www.inta.com" id="amazon_" >Amazon</a> 
+							
+						</div>
+				</div>	
+		
+	<div class="row divider_">
+			<!-- <div class ="mobile-full desktop-8 desktop-push-2 "> !-->
+				<ul class="mobile-full desktop-2 social_ remove_bullet right ">
+					<li id="twitter"> <a href="#" > Twitter   </a> </li>
+					<li id="facebook"> <a href="#"> Facebook  </a> </li>
+					<li id="google_plus"> <a href="#"> Google Plus </a> </li>
+				</ul>
+			
+		
+
+		<div class = "mobile-full desktop-3 footer_ privacy_terms ">
+				<ul class="remove_bullet ">
+					<li class= "list_inline"> Support			 </li>
+					<li class= "list_inline"> Privacy &amp; Terms </li>
+				</ul>
+			</div>
+		
+
+		<div class="mobile-full desktop-2 center">
+			<p class="copyright_"> &copy; 2014 PicticiousPro </p>
+		</div>
+
+	
+	</div>
+		
+	
+	
+	</footer>
+
+
+	<script>
+			var GridlockBookmarkletConfig = {
+				useCookies: true,
+				position: "bottom-left"
+			};
+			(function(){if(typeof GridlockBookmarklet=='undefined'){document.body.appendChild(document.createElement('script')).src='http://formstone.it/js/gridlock.bookmarklet.js';}else{GridlockBookmarklet();}})();
+		</script>
+
+
+					
+
+
+</html>
---
+++ Projects/08-PhotoApp/templates/_header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+	<head>
+		<title>PicticiousPro</title>
+		<meta charset="utf-8">
+		
+		<!-- <link rel="stylesheet" href="components/normalize-css/normalize.css">
+		<link rel="stylesheet" href="components/Gridlock/fs.gridlock.min.css">
+		<link rel="stylesheet" href="css/main.css"> >!-->
+
+		<link rel="stylesheet" href="css/site.min.css">
+		<link href='http://fonts.googleapis.com/css?family=Roboto:400italic,400,700italic,700|Roboto+Condensed:300' rel='stylesheet' type='text/css'>
+
+		<script src="components/modernizr/modernizr.js"></script>
+
+	</head>
+
+	<body class="gridlock">
+		<header class="header"> 
+			<div class = "row ">
+				<div class = "all-full ">
+					<a href="#" class="logo"><strong>Picticious</strong>Pro </a>
+				</div>
+			</div>
+		</header>
